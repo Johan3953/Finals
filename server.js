89a67becaa9a00c9b463ef043541ef8555589e02
Johan3953 <JohanManuel3953@gmail.com>
2018-04-14 22:15:50 -0400

    3.0

--- server.js
+++ server.js
@@ -12,43 +12,55 @@ var chart = require("chart.js");
 //Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 3000;
-
-//Requiring our models for syncing
-var db = require("./models");
-
-//Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
-
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-//Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
-
-//Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-//Routes
-require("./routes/html-routes.js")(app);
-require("./routes/auth-routes.js")(app);
-require("./routes/note-api-routes.js")(app);
-require("./routes/user-api-routes.js")(app);
-
-
-//Syncing our sequelize models and then starting our express app
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
+  //Requiring our models for syncing
+  var db = require("./models");
+
+  //Sets up the Express app to handle data parsing
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.text());
+  app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+  }));
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+
+  //Serve static content for the app from the "public" directory in the application directory.
+  app.use(express.static(process.cwd() + "/public"));
+
+  app.use(session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  //Override with POST having ?_method=DELETE
+  app.use(methodOverride("_method"));
+
+  //Set Handlebars.
+  var exphbs = require("express-handlebars");
+
+  app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+  }));
+  app.set("view engine", "handlebars");
+
+  //Routes
+  require("./routes/html-routes.js")(app);
+  require("./routes/auth-routes.js")(app);
+  require("./routes/note-api-routes.js")(app);
+  require("./routes/user-api-routes.js")(app);
+
+
+  //Syncing our sequelize models and then starting our express app
+
 });
